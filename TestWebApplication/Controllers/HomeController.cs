519b5ab38befc1020d8d4cee87ddced43b0cb64e
Vasyl Shevaha <shevaha201@gmail.com>
2017-08-02 00:21:19 +0300

    Added test application

---
+++ TestWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApplication.Models;
+
+namespace TestWebApplication.Controllers {
+	public class HomeController : Controller {
+
+		public static ICollection<Record> Records = new List<Record>();
+
+		// GET: Home
+		public ActionResult Index() {
+			return View(Records);
+		}
+
+		[HttpGet]
+		public ActionResult AddRecord() {
+			var viewModel = new Record();
+			return PartialView("_AddRecord", viewModel);
+		}
+
+		[HttpPost]
+		public ActionResult AddRecord(Record record) {
+			record.Id = (new Random()).Next(0, 100000);
+			record.CreatedDate = DateTime.Now;
+			Records.Add(record);
+			return PartialView("_RecordItem", record);
+		}
+	}
+}
---
+++ TestWebApplication/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestWebApplication.Models {
+	public class Record {
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Count { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+}
